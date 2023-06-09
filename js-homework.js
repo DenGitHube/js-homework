2dcf649c51e5d7eac4039357fad46cf4832b1df0
Denis <dniproschool@gmail.com>
2023-06-09 23:11:10 +0300

    homework_V2.0

--- js-homework.js
+++ js-homework.js
@@ -1,54 +1,61 @@
 /* --⭐Задача на повернення ініціалів для кожного імені з масиву, посортованих в алфавітному порядку:⭐ -- */
 
-/*const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
-let initials = [];
+/*
 
-for (let i = 0; i < userNames.length; i++) {
-  const name = userNames[i];
-  const parts = name.split(" "); // Розділити рядок імені на окремі частини за допомогою пробілів
-  let initialsForName = "";
+const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
 
-  for (let j = 0; j < parts.length; j++) {
-    const part = parts[j];
-    initialsForName += part.charAt(0) + "."; // Додати першу літеру кожної частини до рядку ініціалів
-  }
+const initials = userNames.map((name) => {      // userNames, містить три рядки з іменами, map створює новий масив з ініціалами
+    return name.split(" ").map((part) => {      // split  розбиття рядка на окремі слова
+      return part.charAt(0) + ".";              // charAt дає нам першу літеру слова та ми додаємо крапку 
+    });
+});                                    
 
-  initials.push(initialsForName); // Додати рядок ініціалів до масиву ініціалів
-}
+console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]   // console.log отримуємо відсортований масив з ініціалами для кожного імені
 
-console.log(initials); // ["П.О.І."", "Г.П.А."", "Р.А.О."] 
+*/
 
-/* --⭐Задача на розворот числа:⭐-- /*
+/* --⭐Задача на розворот числа:⭐-- */
 
-/* const currentMaxValue = 4589;
-let reverseMaxValue; 
+/*
 
-var num = 4589;
+const currentMaxValue = 4589;
 
-function getReversedNum(num) {
-  let result = 0;
-  while (num) {
-    result = result * 10 + num % 10;
-    num = Math.floor(num / 10);
-  }
-  return result;
-}
+let reverseMaxValue = String(currentMaxValue)  // я за допомогою string перетворив число currentMaxValue на рядок
+.split("")  // розбиває рядок на окремі символи і буде містити окремі цифри числа
+.reverse()  // перевертає порядок елементів у масиві
+.join("");  // об'єднує всі елементи, а саме цифри числа назад у рядок
 
-console.log(getReversedNum(num));
-
+const reverseMaxValueNumber = Number(reverseMaxValue); // перетворює рядок назад у число за допомогою Number
 console.log(reverseMaxValue); // 9854
-console.log(typeof reverseMaxValue); // 'number' */ 
+console.log(typeof reverseMaxValue); // 'number' 
+
+*/
 
 /* --⭐Задача на знаходження добутку масиву чисел з невідомою глибиною вкладеності:⭐-- */
 
-/*const resultsArray = [1, 2, [3, [4]]];
+/*
+
+const resultsArray = [1, 2, [3, [4]]];
 let productOfArray;
 
-let mult = 1;
+function calculateProduct(arr) {                    // приймає масив як аргумент і повертає обчислений добуток
+    let product = 1;                                // змінна зі значенням 1
+    
+    for (let i = 0; i < arr.length; i++) {          // перевіряється, чи є arr масивом або числом
+      if (Array.isArray(arr[i])) {                  // Якщо елемент є масивом то викликається функція calculateProduct з масивом arr[i]
+        product *= calculateProduct(arr[i]);        // Результат помножується на змінну product
+      } else {
+        product *= arr[i];                          // Якщо елемент є числом, воно множиться на змінну product
+      }
+    }
+    
+    return product;                                 // Значення змінної product повертається з функції calculateProduct
+  }
+  
+productOfArray = calculateProduct(resultsArray);    
 
-for (let i=1; i<=4; i++) {
-  mult = mult * i;
-}
+console.log(productOfArray); // 24 
 
-console.log(mult);
-console.log(productOfArray); // 24      */
+*/ 
+
+// Буду вдячний якщо ви надасте мені додаткову літературу за допомогою якої я б зміг виконати це завдання краще.
