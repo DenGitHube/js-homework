07a5b5d618e41aa133897bc099bacf9630586b8b
DenGitHube <dendanilo1709@gmail.com>
2023-07-17 00:19:13 +0300

    code reengineering

--- script.js
+++ script.js
@@ -18,104 +18,130 @@ console.log(doubled); // [2, 4, 6, 8, 10]
 */
 
 
-// Базовий клас
+// 1) Клас-предок
 class Entity {
+  // Конструктор класу, приймає параметр name
   constructor(name) {
-    this.name = name; // Властивість - ім'я сутності
-    this._privateProperty = 'Це приватна властивість.'; // Приватна властивість
+    // Присвоюємо значення параметра name до властивості name об'єкта
+    this.name = name;
+    // Встановлюємо значення "Private property" для приватної властивості privateProperty
+    this.privateProperty = "Private property";
   }
 
-  commonMethod() {
-    console.log('Це загальний метод.'); // Загальний метод
+  // Спільний метод, який виводить повідомлення
+  sharedMethod() {
+    console.log("This is a shared method.");
   }
 }
 
-// Клас Тварина успадковується від Entity
+// 2) Класи-сутності (тварини)
 class Animal extends Entity {
-  constructor(name, age, type) {
+  // Конструктор класу, приймає параметри name, age, species
+  constructor(name, age, species) {
+    // Виклик конструктора батьківського класу і передача параметра name
     super(name);
-    this.age = age; // Властивість - вік тварини
-    this.type = type; // Властивість - тип тварини
+    // Присвоюємо значення параметра age до властивості age об'єкта
+    this.age = age;
+    // Присвоюємо значення параметра species до властивості species об'єкта
+    this.species = species;
   }
 
-  sound() {
-    console.log('Ця тварина видає звук.'); // Метод - звук тварини
-  }
-
+  // Метод, який виводить повідомлення про їжу
   eat(food) {
-    console.log(`${this.name} їсть ${food}.`); // Метод - тварина їсть їжу
+    console.log(`${this.name} is eating ${food}.`);
+  }
+
+  // Метод, який виводить повідомлення про сон
+  sleep() {
+    console.log(`${this.name} is sleeping.`);
+  }
+
+  // Унікальний метод для підкласу Animal, який виводить повідомлення про унікальну властивість
+  uniqueProperty() {
+    console.log(`Unique property for ${this.name}`);
   }
 }
 
-// Клас Покемон успадковується від Entity
-class Pokemon extends Entity {
-  constructor(name, level, type) {
-    super(name);
-    this.level = level; // Властивість - рівень покемона
-    this.type = type; // Властивість - тип покемона
-  }
-
-  attack(move) {
-    console.log(`${this.name} атакує за допомогою ${move}.`); // Метод - покемон атакує
-  }
-
-  train() {
-    console.log(`${this.name} тренується.`); // Метод - покемон тренується
-  }
-}
-
-// Клас Гонка успадковується від Entity
-class Race extends Entity {
-  constructor(name, distance, participants) {
-    super(name);
-    this.distance = distance; // Властивість - дистанція гонки
-    this.participants = participants; // Властивість - учасники гонки
-  }
-
-  start() {
-    console.log(`Гонка ${this.name} розпочалась.`); // Метод - початок гонки
-  }
-
-  finish() {
-    console.log(`Гонка ${this.name} закінчилась.`); // Метод - кінець гонки
-  }
-}
-
-// Клас Собака успадковується від Animal
+// Клас-потомок Dog успадковує властивості та методи від Animal
 class Dog extends Animal {
+  // Конструктор класу, приймає параметри name, age, breed
   constructor(name, age, breed) {
-    super(name, age, 'собака');
-    this.breed = breed; // Властивість - порода собаки
+    // Виклик конструктора батьківського класу і передача параметрів name, age, "Dog"
+    super(name, age, "Dog");
+    // Присвоюємо значення параметра breed до властивості breed об'єкта
+    this.breed = breed;
   }
 
+  // Метод, який виводить повідомлення про гавкання
   bark() {
-    console.log(`${this.name} гавкає.`); // Метод - собака гавкає
+    console.log("Woof! Woof!");
+  }
+
+  // Унікальний метод для підкласу Dog
+  uniqueMethod() {
+    console.log(`Unique method for ${this.name}`);
   }
 }
 
-// Клас Пікачу успадковується від Pokemon
-class Pikachu extends Pokemon {
-  constructor(name, level) {
-    super(name, level, 'електричний');
+// Клас-потомок Cat успадковує властивості та методи від Animal
+class Cat extends Animal {
+  // Конструктор класу, приймає параметри name, age, color
+  constructor(name, age, color) {
+    // Виклик конструктора батьківського класу і передача параметрів name, age, "Cat"
+    super(name, age, "Cat");
+    // Присвоюємо значення параметра color до властивості color об'єкта
+    this.color = color;
   }
 
-  thunderbolt() {
-    console.log(`${this.name} використав Удар Громом!`); // Метод - Пікачу використовує Удар Громом
+  // Метод, який виводить повідомлення про мяукання
+  meow() {
+    console.log("Meow!");
+  }
+
+  // Унікальний метод для підкласу Cat
+  uniqueMethod() {
+    console.log(`Unique method for ${this.name}`);
   }
 }
 
-// Використання класів
-var dog = new Dog('Бадді', 3, 'Золотистий ретрівер');
-dog.bark(); // Виведе: Бадді гавкає.
-dog.eat('кістку'); // Виведе: Бадді їсть кістку.
-dog.commonMethod(); // Виведе: Це загальний метод.
+// Клас-потомок Bird успадковує властивості та методи від Animal
+class Bird extends Animal {
+  // Конструктор класу, приймає параметри name, age, type
+  constructor(name, age, type) {
+    // Виклик конструктора батьківського класу і передача параметрів name, age, "Bird"
+    super(name, age, "Bird");
+    // Присвоюємо значення параметра type до властивості type об'єкта
+    this.type = type;
+  }
 
-var pikachu = new Pikachu('Піка', 25);
-pikachu.attack('Електричний удар'); // Виведе: Піка атакує за допомогою Електричного удару.
-pikachu.thunderbolt(); // Виведе: Піка використав Удар Громом!
-pikachu.commonMethod(); // Виведе: Це загальний метод.
+  // Метод, який виводить повідомлення про польот
+  fly() {
+    console.log(`${this.name} is flying.`);
+  }
 
-var race = new Race('Спринт', '100 м', ['Учасник1', 'Учасник2']);
-race.start(); // Виведе: Гонка Спринт розпочалась.
-race.finish(); // Виведе: Гонка Спринт закінчилась.
-race.commonMethod(); // Виведе: Це загальний метод.
+  // Унікальний метод для підкласу Bird
+  uniqueMethod() {
+    console.log(`Unique method for ${this.name}`);
+  }
+}
+
+// Приклад використання створених класів і об'єктів
+const dog = new Dog("Buddy", 3, "Labrador");
+const cat = new Cat("Whiskers", 5, "Orange");
+const bird = new Bird("Tweety", 2, "Canary");
+
+dog.sharedMethod(); // Виклик спільного методу класу Entity
+cat.sharedMethod(); // Виклик спільного методу класу Entity
+
+dog.bark(); // Метод тільки для собаки
+cat.meow(); // Метод тільки для кота
+bird.fly(); // Метод тільки для птаха
+
+dog.uniqueMethod(); // Унікальний метод для собаки
+cat.uniqueMethod(); // Унікальний метод для кота
+bird.uniqueMethod(); // Унікальний метод для птаха
+
+console.log(dog.privateProperty); // Доступ до приватної властивості класу Entity
+console.log(cat.privateProperty); // Доступ до приватної властивості класу Entity
+console.log(bird.privateProperty); // Доступ до приватної властивості класу Entity
+
