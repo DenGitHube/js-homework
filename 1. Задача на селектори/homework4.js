8b5fd94c6adce92566283804bda2ee414b0a904c
DenGitHube <dendanilo1709@gmail.com>
2023-07-16 19:57:05 +0300

    fix Selectors

--- 1. Задача на селектори/homework4.js
+++ 1. Задача на селектори/homework4.js
@@ -1,8 +1,7 @@
-// Створені селектори
-let navigationText = document.querySelector('#headerTwo');          // для елементу з текстом 'Навігація по DOM дереву'
-let firstSection = document.querySelector('section:first-of-type'); // для першого елементу <section>
-let listItem = document.querySelector('li:nth-child(5)');           // для елементу списку з текстом 'Пункт 5'
-let elementWithClass = document.querySelector('.hatredLevelBlock'); // для елементу з класом 'hatredLevelBlock'
+let navigationText = document.getElementById('headerTwo');                  // для елементу з текстом 'Навігація по DOM дереву'
+let firstSection = document.querySelector('section:first-of-type');         // для першого елементу <section>
+let listItem = document.querySelector('li:nth-child(5)');                   // для елементу списку з текстом 'Пункт 5'
+let elementWithClass = document.getElementsByClassName('hatredLevelBlock')[0]; // для елементу з класом 'hatredLevelBlock'
 
 // Запуск та використання цих селекторів 
 console.log(navigationText.textContent);
