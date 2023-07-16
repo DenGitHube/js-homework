ca7a22bfd0a2b9e0e7e956af8103b1109415e9ce
DenGitHube <dendanilo1709@gmail.com>
2023-07-16 23:36:20 +0300

    delete one querySelector

--- 1. Задача на селектори/homework4.js
+++ 1. Задача на селектори/homework4.js
@@ -1,6 +1,6 @@
 let navigationText = document.getElementById('headerTwo');                  // для елементу з текстом 'Навігація по DOM дереву'
 let firstSection = document.querySelector('section:first-of-type');         // для першого елементу <section>
-let listItem = document.querySelector('li:nth-child(5)');                   // для елементу списку з текстом 'Пункт 5'
+let listItem = document.getElementsByTagName('li')[4];                      // для елементу списку з текстом 'Пункт 5'
 let elementWithClass = document.getElementsByClassName('hatredLevelBlock')[0]; // для елементу з класом 'hatredLevelBlock'
 
 // Запуск та використання цих селекторів 
@@ -8,3 +8,4 @@ console.log(navigationText.textContent);
 console.log(firstSection);
 console.log(listItem.textContent);    
 console.log(elementWithClass); 
+
