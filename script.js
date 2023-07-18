f314cd7633a293bd2e48a51ee1a80e6c3ba164d7
DenGitHube <dendanilo1709@gmail.com>
2023-07-18 17:56:07 +0300

    Final version of course project 1

--- script.js
+++ script.js
@@ -2,94 +2,106 @@
 
 // Функція для збереження результату в локальне сховище
 function saveResult(startDate, endDate, result) {
-  // Отримуємо попередні результати з локального сховища або створюємо новий пустий масив
-  const results = JSON.parse(localStorage.getItem('results')) || []; 
-  // Додаємо новий результат до масиву
+  if (typeof Storage === 'undefined') {
+    alert('Вибачте, але ваш браузер не підтримує локальне сховище.');
+    return;
+  }
+
+  // Отримання результатів з локального сховища або створення нового масиву
+  const results = JSON.parse(localStorage.getItem('results')) || [];
+  // Додавання нового результату до масиву результатів
   results.push({ startDate, endDate, result });
-  // Зберігаємо оновлений масив результатів у локальне сховище, перетворивши його на рядок JSON
+  // Збереження оновленого масиву результатів у локальному сховищі
   localStorage.setItem('results', JSON.stringify(results));
 }
 
 // Функція для завантаження результатів з локального сховища і відображення їх у таблиці
 function loadResults() {
-  // Отримуємо результати з локального сховища або створюємо новий пустий масив
+  // Перевірка підтримки локального сховища в браузері
+  if (typeof Storage === 'undefined') {
+    alert('Вибачте, але ваш браузер не підтримує локальне сховище.');
+    return;
+  }
+
+  // Отримання результатів з локального сховища або створення порожнього масиву
   const results = JSON.parse(localStorage.getItem('results')) || [];
-  // Отримуємо посилання на HTML-таблицю за допомогою ідентифікатора
+  // Отримання посилання на таблицю результатів за допомогою її ідентифікатора
   const table = document.getElementById('results-table');
+  // Очищення вмісту таблиці
+  table.innerHTML = '';
 
-  // Проходимося по кожному результату і додаємо його до таблиці
+  // Перевірка, чи є результати для відображення
+  if (results.length === 0) {
+    return;
+  }
+
+  // Прохід по масиву результатів та додавання їх у таблицю
   for (let i = 0; i < results.length; i++) {
-    const newRow = table.insertRow(-1); // Створюємо новий рядок в таблиці
+    // Створення нового рядка у таблиці
+    const newRow = table.insertRow(-1);
+    // Створення комірок для початкової дати, кінцевої дати, результату та кнопки видалення
+    const startDateCell = newRow.insertCell(0);
+    const endDateCell = newRow.insertCell(1);
+    const resultCell = newRow.insertCell(2);
+    const deleteCell = newRow.insertCell(3);
 
-    // Створюємо комірки для початкової дати, кінцевої дати, результату і кнопки видалення
-    const startDateCell = newRow.insertCell(0); // Створюємо комірку для початкової дати
-    const endDateCell = newRow.insertCell(1);   // Створюємо комірку для кінцевої дати
-    const resultCell = newRow.insertCell(2);    // Створюємо комірку для результату
-    const deleteCell = newRow.insertCell(3);    // Створюємо комірку для кнопки видалення
+    // Заповнення комірок значеннями з масиву результатів
+    startDateCell.innerHTML = results[i].startDate;
+    endDateCell.innerHTML = results[i].endDate;
+    resultCell.innerHTML = results[i].result;
 
-    // Заповнюємо комірки значеннями з результатів
-    startDateCell.innerHTML = results[i].startDate; // Заповнюємо комірку початкової дати значенням з результатів
-    endDateCell.innerHTML = results[i].endDate;     // Заповнюємо комірку кінцевої дати значенням з результатів
-    resultCell.innerHTML = results[i].result;       // Заповнюємо комірку результату значенням з результатів;
-
-    // Створюємо кнопку видалення
-    const deleteButton = document.createElement('button'); // Створюємо кнопку видалення
+    // Створення кнопки видалення
+    const deleteButton = document.createElement('button');
     deleteButton.innerHTML = 'Delete 🗑️';
-    deleteButton.classList.add('delete-button'); // Додаємо клас до кнопки видалення
-    deleteButton.setAttribute('data-index', i); // Встановлюємо атрибут "data-index" кнопки видалення з індексом результату
-    deleteButton.addEventListener('click', deleteResult); // Додаємо обробник події на кнопку видалення
+    deleteButton.classList.add('delete-button');
+    deleteButton.setAttribute('data-index', i);
+    deleteButton.addEventListener('click', deleteResult);
 
-    // Додаємо кнопку видалення до комірки видалення
+    // Додавання кнопки видалення до комірки видалення
     deleteCell.appendChild(deleteButton);
   }
 }
 
-// ⚠️⚠️ Функція для видалення результату ⚠️⚠️
+// Функція для видалення результату
 function deleteResult(event) {
-  // Отримуємо індекс результату, який потрібно видалити
+  // Отримання індексу результату, який потрібно видалити
   const index = event.target.getAttribute('data-index');
-  // Отримуємо результати з локального сховища
+  // Отримання масиву результатів з локального сховища або створення порожнього масиву
   const results = JSON.parse(localStorage.getItem('results')) || [];
-  // Видаляємо результат з масиву за його індексом
+  // Видалення результату з масиву за допомогою індексу
   results.splice(index, 1);
-  // Оновлюємо локальне сховище, зберігаючи оновлений масив результатів
+  // Збереження оновленого масиву результатів у локальному сховищі
   localStorage.setItem('results', JSON.stringify(results));
 
-  // Отримуємо посилання на таблицю
-  const table = document.getElementById('results-table');
-  // Видаляємо рядок таблиці, на якому була натиснута кнопка видалення
-  table.deleteRow(event.target.parentNode.parentNode.rowIndex);
+  // Поновлення відображення результатів у таблиці
+  loadResults();
 }
 
-// ⚠️⚠️ Функція для обчислення різниці в часі ⚠️⚠️
+// Функція для обчислення різниці між двома датами
 function calculateTime() {
-  const startDate = document.getElementById('start-date').value; // Отримуємо значення початкової дати з HTML-форми
-  const endDate = document.getElementById('end-date').value; // Отримуємо значення кінцевої дати з HTML-форми
-  //const preset = document.getElementById('preset').value;
-  //const options = document.getElementById('options').value;
-  //const measure = document.getElementById('measure').value; // Отримуємо значення "measure" з HTML-форми
+  // Отримання значень початкової дати, кінцевої дати, одиниці виміру та опцій з відповідних елементів форми
+  const startDate = document.getElementById('start-date').value;
+  const endDate = document.getElementById('end-date').value;
+  const measure = document.getElementById('measure').value;
+  const options = document.getElementById('options').value;
 
-  // ⚠️⚠️ Перевірка на правильність дат ⚠️⚠️
-  if (startDate > endDate) {
-    alert('Початкова дата повинна бути раніше або рівною кінцевій даті');
+  // Перевірка, чи початкова та кінцева дати не є однаковими
+  if (endDate === startDate) {
+    alert('Початкова і кінцева дати не можуть бути однаковими.');
     return;
   }
 
-  const measure = document.getElementById('measure').value;
-
-  // Конвертуємо початкову та кінцеву дати в об'єкти JavaScript Date і отримуємо їх час в мілісекундах
-  const startTime = new Date(startDate).getTime(); 
-  const endTime = new Date(endDate).getTime(); 
-
-  // Обчислюємо різницю в часі
+  // Перетворення початкової та кінцевої дат у мілісекунди
+  const startTime = new Date(startDate).getTime();
+  const endTime = new Date(endDate).getTime();
+  // Обчислення різниці часу між датами
   const timeDiff = endTime - startTime;
 
-  // Ініціалізуємо змінні для результату та одиниці виміру
   let result;
   let unit;
 
-  // Вибираємо правильний розрахунок та одиницю виміру залежно від значення "measure"
-  switch (measure) { 
+  // Вирахування результату та одиниці виміру в залежності від обраної одиниці виміру
+  switch (measure) {
     case 'days':
       result = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
       unit = 'днів';
@@ -108,37 +120,168 @@ function calculateTime() {
       break;
   }
 
-  // Отримуємо посилання на таблицю
+  // Перевірка обраної опції та модифікація результату та одиниці виміру, якщо потрібно
+  if (options === 'weekdays') {
+    const startDateObj = new Date(startDate);
+    const endDateObj = new Date(endDate);
+    const weekdays = getWeekdaysBetweenDates(startDateObj, endDateObj);
+    result = weekdays.length;
+
+    // Модифікація результату та одиниці виміру для вибраної опції "Budnі dnі"
+    if (measure === 'hours') {
+      result *= 24;
+      unit = 'годин';
+    } else if (measure === 'minutes') {
+      result *= 24 * 60;
+      unit = 'хвилин';
+    } else if (measure === 'seconds') {
+      result *= 24 * 60 * 60;
+      unit = 'секунд';
+    }
+  } else if (options === 'weekends') {
+    const startDateObj = new Date(startDate);
+    const endDateObj = new Date(endDate);
+    const weekends = getWeekendsBetweenDates(startDateObj, endDateObj);
+    result = weekends.length;
+
+    // Модифікація результату та одиниці виміру для вибраної опції "Vikhіdnі dnі"
+    if (measure === 'hours') {
+      result *= 24;
+      unit = 'годин';
+    } else if (measure === 'minutes') {
+      result *= 24 * 60;
+      unit = 'хвилин';
+    } else if (measure === 'seconds') {
+      result *= 24 * 60 * 60;
+      unit = 'секунд';
+    }
+  }
+
+  // Отримання посилання на таблицю результатів за допомогою її ідентифікатора
   const table = document.getElementById('results-table');
-  // Створюємо новий рядок в таблиці
+  // Створення нового рядка у таблиці
   const newRow = table.insertRow(-1);
 
-  // Створюємо комірки для початкової дати, кінцевої дати, результату і кнопки видалення
-  const startDateCell = newRow.insertCell(0); // Створюємо комірку для початкової дати
-  const endDateCell = newRow.insertCell(1);   // Створюємо комірку для кінцевої дати
-  const resultCell = newRow.insertCell(2);    // Створюємо комірку для результату
-  const deleteCell = newRow.insertCell(3);    // Створюємо комірку для кнопки видалення
+  // Створення комірок для початкової дати, кінцевої дати, результату та кнопки видалення
+  const startDateCell = newRow.insertCell(0);
+  const endDateCell = newRow.insertCell(1);
+  const resultCell = newRow.insertCell(2);
+  const deleteCell = newRow.insertCell(3);
 
-  // Заповнюємо комірки значеннями з результатів
-  startDateCell.innerHTML = startDate;        // Заповнюємо комірку початкової дати значенням з форми
-  endDateCell.innerHTML = endDate;            // Заповнюємо комірку кінцевої дати значенням з форми
-  resultCell.innerHTML = result + ' ' + unit; // Заповнюємо комірку результату
+  // Заповнення комірок значеннями результату
+  startDateCell.innerHTML = startDate;
+  endDateCell.innerHTML = endDate;
+  resultCell.innerHTML = result + ' ' + unit;
 
-  // Створюємо кнопку видалення
-  const deleteButton = document.createElement('button');  // Створюємо кнопку видалення
-  deleteButton.innerHTML = 'Delete 🗑️'; 
-  deleteButton.classList.add('delete-button');            // Додаємо клас до кнопки видалення
-  deleteButton.setAttribute('data-index', table.rows.length - 2); // Встановлюємо атрибут "data-index" кнопки видалення з індексом останнього рядка таблиці
-  deleteButton.addEventListener('click', deleteResult);   // Додаємо обробник події на кнопку видалення
+  // Створення кнопки видаленн
+  const deleteButton = document.createElement('button');
+  deleteButton.innerHTML = 'Delete 🗑️';
+  deleteButton.classList.add('delete-button');
+  deleteButton.setAttribute('data-index', table.rows.length - 2);
+  deleteButton.addEventListener('click', deleteResult);
 
-  // Додаємо кнопку видалення до комірки видалення
+  // Додавання кнопки видалення до комірки видалення
   deleteCell.appendChild(deleteButton);
 
-  // Зберігаємо результат в локальне сховище
+  // Збереження результату в локальному сховищі
   saveResult(startDate, endDate, result + ' ' + unit);
 }
 
-// Функція, яка виконується при завантаженні сторінки
+// Функція для оновлення мінімального значення кінцевої дати
+function updateEndDateMin() {
+  // Отримання посилань на елементи вводу початкової та кінцевої дат
+  const startDateInput = document.getElementById('start-date');
+  const endDateInput = document.getElementById('end-date');
+
+  // Перевірка, чи співпадають значення початкової та кінцевої дат
+  if (endDateInput.value === startDateInput.value) {
+    endDateInput.value = '';
+  }
+
+  // Встановлення мінімального значення для кінцевої дати
+  endDateInput.min = startDateInput.value;
+}
+
+// Обробник події завантаження сторінки
 document.addEventListener('DOMContentLoaded', function () {
-  loadResults(); // Завантажуємо результати з локального сховища і відображаємо їх у таблиці
+  // Отримання посилань на елементи вводу початкової та кінцевої дат
+  const startDateInput = document.getElementById('start-date');
+  const endDateInput = document.getElementById('end-date');
+
+  // Додавання обробників подій для валідації дат
+  startDateInput.addEventListener('input', updateEndDateMin);
+  endDateInput.addEventListener('input', function () {
+    if (endDateInput.value < endDateInput.min) {
+      endDateInput.value = '';
+    }
+  });
+
+  // Отримання посилання на елемент вибору пресету
+  const presetSelect = document.getElementById('preset');
+  // Додавання обробника події для зміни вибраного пресету
+  presetSelect.addEventListener('change', handlePresetChange);
+
+  // Завантаження результатів з локального сховища та відображення їх у таблиці
+  loadResults();
 });
+
+// Функція для обробки зміни вибраного пресету
+function handlePresetChange() {
+  // Отримання значення вибраного пресету
+  const presetValue = document.getElementById('preset').value;
+  // Отримання посилань на елементи вводу початкової та кінцевої дат
+  const startDateInput = document.getElementById('start-date');
+  const endDateInput = document.getElementById('end-date');
+  // Створення об'єкта з поточною датою
+  const currentDate = new Date();
+  // Встановлення значення початкової дати як поточної дати
+  startDateInput.valueAsDate = currentDate;
+
+  let endDate;
+
+  // Вибір кінцевої дати в залежності від вибраного пресету
+  switch (presetValue) {
+    case 'week':
+      endDate = new Date(currentDate.getTime() + 7 * 24 * 60 * 60 * 1000);
+      break;
+    case 'month':
+      endDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, currentDate.getDate());
+      break;
+    default:
+      endDate = currentDate;
+      break;
+  }
+
+  // Встановлення значення кінцевої дати
+  endDateInput.valueAsDate = endDate;
+}
+
+// Функція для отримання робочих днів між двома датами
+function getWeekdaysBetweenDates(startDate, endDate) {
+  const weekdays = [];
+  const currentDate = new Date(startDate);
+
+  while (currentDate <= endDate) {
+    if (currentDate.getDay() >= 1 && currentDate.getDay() <= 5) {
+      weekdays.push(new Date(currentDate));
+    }
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return weekdays;
+}
+
+// Функція для отримання вихідних днів між двома датами
+function getWeekendsBetweenDates(startDate, endDate) {
+  const weekends = [];
+  const currentDate = new Date(startDate);
+
+  while (currentDate <= endDate) {
+    if (currentDate.getDay() === 0 || currentDate.getDay() === 6) {
+      weekends.push(new Date(currentDate));
+    }
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return weekends;
+}
